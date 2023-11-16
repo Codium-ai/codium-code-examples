8e6cc917a925bf0fd7667024f7f1b42cadb30eb2
Shai Geva <6005026+shaigeva@users.noreply.github.com>
2023-11-16 13:03:20 +0200

    Add property-based testing example

--- examples/quick_sort/test_quick_sort.py
+++ examples/quick_sort/test_quick_sort.py
@@ -1,4 +1,7 @@
 import pytest
+from hypothesis import given
+import hypothesis.strategies as some
+
 from examples.quick_sort.quick_sort import qsort
 
 class TestQuickSort:
@@ -12,3 +15,8 @@ class TestQuickSort:
     def test_sort_simple_lists(self, l, expected):
         expected_copy = expected[:]
         assert qsort(l) == expected_copy
+
+class TestQuickSortPBT:
+    @given(some.lists(some.integers()))
+    def test_sort_is_idempotent(self, lst):
+        assert qsort(qsort(lst)) == qsort(lst)
